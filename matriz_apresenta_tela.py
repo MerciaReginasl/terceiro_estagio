71536ab2ff74bc2d8edc7bc655f80809711070b2
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2024-05-07 23:30:47 -0300

    Create matriz_apresenta_tela.py

---
+++ matriz_apresenta_tela.py
@@ -0,0 +1,13 @@
+#Exibindo a matriz na tela:
+turma = [[5.0, 4.5, 7.0, 5.2, 6.1],
+[2.1, 6.5, 8.0, 7.0, 6.7],
+[8.6, 7.0, 9.1, 8.7, 9.3]]
+# Exibindo toda a matriz
+print(turma)
+# Exibindo apenas o primeiro elemento (primeira linha)
+da matriz
+print(turma[0]) # [5.0, 4.5, 7.0, 5.2, 6.1]
+# Exibindo apenas o primeiro elemento do primeiro
+# elemento da matriz – primeiro elemento da primeira
+# linha da matriz
+print(turma[0][0]) # 5.0
