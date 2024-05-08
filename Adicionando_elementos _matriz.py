5bf82cf54bfc2aa0547f46df412c09698a067d19
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2024-05-07 23:34:01 -0300

    Create Adicionando_elementos _matriz.py

---
+++ Adicionando_elementos _matriz.py
@@ -0,0 +1,40 @@
+#Adicionando elementos na matriz:
+# inicializando matriz com 3 linhas e 5 colunas
+turma = [
+[5.0, 4.5, 7.0, 5.2, 6.1],
+[2.1, 6.5, 8.0, 7.0, 6.7],
+[8.6, 7.0, 9.1, 8.7, 9.3]
+]
+# adicionando notas de um quarto aluno
+turma.append([5.5, 6.0, 4.1, 7.7, 8.2])
+# adicionando uma sexta nota no segundo aluno
+turma[1].append(9.4)
+# mostrando toda a matriz atualizada
+print(turma)
+
+turma = [
+[5.0, 4.5, 7.0, 5.2, 6.1],
+[2.1, 6.5, 8.0, 7.0, 6.7],
+[8.6, 7.0, 9.1, 8.7, 9.3],
+[5.5, 6.0, 4.1, 7.7, 8.2]
+]
+# adicionando notas de um quarto aluno
+turma.append([5.5, 6.0, 4.1, 7.7, 8.2])
+# adicionando uma sexta nota no segundo aluno
+turma[1].append(9.4)
+# mostrando toda a matriz atualizada
+print(turma)
+
+turma = [
+[5.0, 4.5, 7.0, 5.2, 6.1],
+[2.1, 6.5, 8.0, 7.0, 6.7, 9.4],
+[8.6, 7.0, 9.1, 8.7, 9.3],
+[5.5, 6.0, 4.1, 7.7, 8.2]
+]
+# adicionando notas de um quarto aluno
+turma.append([5.5, 6.0, 4.1, 7.7, 8.2])
+# adicionando uma sexta nota no segundo aluno
+turma[1].append(9.4)
+# mostrando toda a matriz atualizada
+print(turma)
+OPERAÇÕ
