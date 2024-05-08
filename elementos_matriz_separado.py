c4221644560fbd94b51747af5445858f41a62968
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2024-05-07 23:32:30 -0300

    Create elementos_matriz_separado.py

---
+++ elementos_matriz_separado.py
@@ -0,0 +1,10 @@
+#Acessando elementos da matriz separadamente:
+turma = [
+[5.0, 4.5, 7.0, 5.2, 6.1],
+[2.1, 6.5, 8.0, 7.0, 6.7],
+[8.6, 7.0, 9.1, 8.7, 9.3]
+]
+# Qual a terceira nota do primeiro aluno?
+turma[0][2] # 7.0
+# Qual a primeira nota do terceiro aluno?
+turma[2][0] # 8.6
